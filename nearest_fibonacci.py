e24becca1395ce4252cfb2d0a8e6c688e82ce1ad
shreeramsingh1707 <97613903+shreeramsingh1707@users.noreply.github.com>
2022-06-30 23:46:21 +0530

    n=int(input()) a=0 b=1 while(a<=n):     i=a     c=a+b     a=b     b=c     s=a x=abs(n-i) y=abs(n-s) if(x>y):     print(s) elif(x==y):     print(i,s) else:     print(i)

---
+++ nearest_fibonacci.py
@@ -0,0 +1,17 @@
+n=int(input())
+a=0
+b=1
+while(a<=n):
+    i=a
+    c=a+b
+    a=b
+    b=c
+    s=a
+x=abs(n-i)
+y=abs(n-s)
+if(x>y):
+    print(s)
+elif(x==y):
+    print(i,s)
+else:
+    print(i)
