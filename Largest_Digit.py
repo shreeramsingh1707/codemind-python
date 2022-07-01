15b31512b2896fbbc1f4cf1f0a8e2bcba181cf84
shreeramsingh1707 <97613903+shreeramsingh1707@users.noreply.github.com>
2022-07-01 06:09:00 +0530

    num=int(input()) ma=0 while num>0:     digit=num%10     if ma<digit:         ma=digit     num=num//10 print(ma)

--- Largest_Digit.py
+++ Largest_Digit.py
@@ -1,9 +1,8 @@
-n=int(input())
-large=0
-while n>0:
-    r=n%10
-    if large<r:
-        large=r
-    n=n//10
-print(large)
-    
+num=int(input())
+ma=0
+while num>0:
+    digit=num%10
+    if ma<digit:
+        ma=digit
+    num=num//10
+print(ma)
